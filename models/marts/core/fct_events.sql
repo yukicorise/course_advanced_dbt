9775531d387cdd3e6a60487b1369185a5e56cb00
yukicorise <yuki551613@hotmail.com>
2023-08-18 22:11:28 +0000

    make fct_events.sql incremental, add dbt_snowflake_monitoring to identify top 10 expensive queries

--- models/marts/core/fct_events.sql
+++ models/marts/core/fct_events.sql
@@ -1,9 +1,26 @@
 {{
-  config(
-    materialized='table'
-  )
+    config(
+        materialized='incremental',
+        unique_key='event_id'
+    )
 }}
 
-SELECT
-    *
-FROM {{ ref('stg_bingeflix_events') }}
+WITH source AS (
+
+  SELECT
+    event_id,
+    session_id,
+    user_id,
+    created_at,
+    event_name
+  FROM {{ ref('stg_bingeflix_events') }}
+
+{% if is_incremental() %}
+
+  {{ incremental_where_clause(column_name = 'created_at', lookback_window = -3, periods = 'day') }}
+
+{% endif %}
+
+)
+
+SELECT * FROM source
